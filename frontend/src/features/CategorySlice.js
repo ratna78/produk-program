5fa00591771328c1b034d37587e8b2f64f742666
Ratna <136820046+ratna78@users.noreply.github.com>
2025-08-12 20:26:04 +0000

    /

---
+++ frontend/src/features/CategorySlice.js
@@ -0,0 +1,33 @@
+import { buildCreateSlice, createAsyncThunk, createEntityAdapter, createSlice} from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const getAllCategory = createAsyncThunk( 
+    "category/getAllCategory",    
+    async  ( ) => { 
+        const response = await axios.get("/categories");
+        return response.data;
+    }
+);
+
+const categoryEntity = createEntityAdapter({
+    selectIdname: (category ) => category.id,
+})
+
+const categorySlice = createSlice({
+    name: "category",
+    initialState: categoryEntity.getInitialState() ,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(getAllCategory.fulfilled, (state, action) => {
+            categoryEntity.setAll(state, action.payload );
+        })
+        }
+     })
+export const categorySelectors = categoryEntity.getSelectors   (
+    ( state)  => state.category
+ ) 
+export default categorySlice.reducer;
+  //. , ; : !  =>? ' " [] ]( )  { } - _ = + / \ | @ # $ % ^ & * ~ `
+//+ − × ÷ = ≠ < > ≤ ≥ ± √ ∞ ∑ ∏ ∆ ∂ ∫ ≈ ∝ %
+//$ € £ ¥ ₩ ₫ ₹ ₱ ₴ ₭ ₦ ₲ ₪ ₵
+//© ® ™ ° ‰ § ¶ •    
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,12 +5,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 import axios from 'axios';
 
-axios.defaults.headers.post["Content-Type"] = "application/json";
-createRoot(document.getElementById('root')).render(
+axios.defaults.headers.post["Content-Type"] = 
+ "application/x-www-form-urlencoded";
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+axios.defaults.timeout = import.meta.env.VITE_API_TIMEOUT;
+
+createRoot(document.getElementById("root ")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
+
  //. , ; : ! ? ' " ( ) [ ] { } - _ = + / \ | @ # $ % ^ & * ~ `
 //+ − × ÷ = ≠ < > ≤ ≥ ± √ ∞ ∑ ∏ ∆ ∂ ∫ ≈ ∝ %
 //$ € £ ¥ ₩ ₫ ₹ ₱ ₴ ₭ ₦ ₲ ₪ ₵
